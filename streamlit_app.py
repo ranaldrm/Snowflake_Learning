26f492a1948846dcffd8554f53de218c413a480d
ranaldrm <117989421+ranaldrm@users.noreply.github.com>
2026-03-10 16:25:51 +0000

    Update session management in streamlit_app.pyupdated for SniS
    Replace get_active_session() with a connection object for session management.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 from snowflake.snowpark.functions import col
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -18,8 +18,9 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 
 
+cnx = st.connection("snowflake")
 
-session = get_active_session()
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
